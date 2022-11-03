040caf758981150db1d83ee52b96b9f66f4a6b56
Hatice-Altun <van.altun@outlook.com>
2022-11-03 14:48:22 +0100

    AreaCalculation

---
+++ src/com/altun/AreaCalculation/Main.java
@@ -0,0 +1,14 @@
+package com.altun.AreaCalculation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Area Calculation
+
+        double roomWidth = 5.55;
+        double roomLength = 4.5;
+        double roomArea = roomWidth * roomLength;
+
+        System.out.println("Total area of the room is: " + roomArea);
+    }
+}
